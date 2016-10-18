cdd3ff48553c0585dbbebb7407f827a1f2459b4a
thomyap <thomyap02@gmail.com>
2016-10-18 17:08:13 +0700

    latihan_2_1 selesai

--- Selasa_141111201_ThomasYaputra/Latihan_2_1/Form1.cs
+++ Selasa_141111201_ThomasYaputra/Latihan_2_1/Form1.cs
@@ -16,5 +16,39 @@ namespace Latihan_2_1
         {
             InitializeComponent();
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            domainUpDown1.SelectedIndex = 0;
+            DateTime tglLahir = new DateTime(1996, 2, 2);
+            DateTime weekend = new DateTime(2016, 1, 1);
+            while(weekend.Year == 2016)
+            {
+                if (weekend.DayOfWeek == DayOfWeek.Saturday || weekend.DayOfWeek == DayOfWeek.Sunday)
+                    monthCalendar1.AddBoldedDate(weekend);
+                weekend = weekend.AddDays(1);
+            }
+            monthCalendar1.AddAnnuallyBoldedDate(tglLahir);
+            monthCalendar1.UpdateBoldedDates();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int tgl = Convert.ToInt32(numericUpDown1.Value);
+            int bln = Convert.ToInt32(domainUpDown1.SelectedIndex) + 1;
+            if (bln == 2 && tgl > 29)
+                MessageBox.Show("Salah!");
+            DateTime lbr = new DateTime(2016, bln, tgl);
+            monthCalendar1.AddAnnuallyBoldedDate(lbr);
+            monthCalendar1.SetDate(lbr);
+            monthCalendar1.UpdateBoldedDates();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DateTime tgl = monthCalendar1.SelectionRange.Start.Date;
+            monthCalendar1.RemoveAnnuallyBoldedDate(tgl);
+            monthCalendar1.UpdateBoldedDates();
+        }
     }
 }
